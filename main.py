fdc70e656263b7e6509caa338381cf72c9b7fee8
EltonTML <ERD534650@tatamotors.com>
2025-05-07 12:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def fetch_posts():
         response.raise_for_status()
         posts = response.json()
         for post in posts[:5]:
-            print(f"Post ID: {post['id']}")
-            print(f"Title: {post['title']}")
-            print(f"Body: {post['body']}\n")
+            print(f"Post ID: {post['id1']}")
+            print(f"Title: {post['title1']}")
+            print(f"Body: {post['body1']}\n")
     except requests.exceptions.RequestException as e:
         print(f"Error fetching posts: {e}")
 
